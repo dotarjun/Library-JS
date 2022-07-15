d0bada179008da910ce57ff5a1448e8c8e25786c
Arjun <arjunsingh8112@gmail.com>
2022-07-15 16:32:11 +0530

    refactored code and made bookCardCreator function

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ let myLibrary = [
     }
 ];
 
-
-
-
 function Book(title, author, pages, status) {
     this.title = title
     this.author = author
@@ -32,24 +29,36 @@ function Book(title, author, pages, status) {
     }
 }
 
+function bookCardCreator(bookSource) {
+    const bookCard = document.createElement('div');
+    const bookTitle = document.createElement('h2')
+    const bookAuthor = document.createElement('h3')
+    const bookPages = document.createElement('p')
+    const bookStatus = document.createElement('p')
 
+    bookTitle.textContent = bookSource.title;
+    bookAuthor.textContent = bookSource.author;
+    bookPages.textContent = bookSource.pages;
 
+    if (bookSource.checked || bookSource.status) {
+        bookStatus.textContent = "Read";
+    } else {
+        bookStatus.textContent = "Not Read";
+    }
 
-
-// Add a “NEW BOOK” button that brings up a form allowing users to input the details
-// for the new book: author, title, number of pages, whether it’s been read and 
-// anything else you might want.
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookStatus);
+    document.body.appendChild(bookCard)
+}
 
 const newBookButton = document.querySelector('.new-book')
 
-
-
 newBookButton.addEventListener('click', function () {
-
     if (document.querySelector('form')) {
         document.body.querySelector('form').style.display = 'initial'
     }
-
     if (!document.querySelector('form')) {
         const bookForm = document.createElement('form');
         bookForm.setAttribute("onsubmit", "return false");
@@ -72,112 +81,38 @@ newBookButton.addEventListener('click', function () {
         inputLabelCreator("text", "title", true, "Book Title: ")
         inputLabelCreator("text", "author", true, "Author: ")
         inputLabelCreator("number", "pages", true, "Pages: ")
-        inputLabelCreator("checkbox", "status", false, "Read ")
-
-
+        inputLabelCreator("checkbox", "status", "", "Read ")
         const submitButton = document.createElement('button')
         submitButton.textContent = 'Add Book'
         submitButton.setAttribute("type", "submit")
         bookForm.appendChild(submitButton);
         document.body.appendChild(bookForm)
-
-
-
-
+        console.log(document.getElementById('status').checked)
 
         submitButton.addEventListener('click', function () {
-
-            const bookCard = document.createElement('div');
-            const bookTitle = document.createElement('h2')
-            const bookAuthor = document.createElement('h3')
-            const bookPages = document.createElement('p')
-            const bookStatus = document.createElement('p')
-
-
+            console.log(document.getElementById('status').checked)
             const formDataAcquired = Array
                 .from(document.querySelectorAll('form input'))
                 .reduce((acc, input) =>
                     ({ ...acc, [input.id]: input.value }), {});
 
+            // formDataAcquired.status = document.getElementById('status').checked
+            // console.log(formDataAcquired);
+            // console.log(formDataAcquired.checked);
+            // console.log(document.getElementById('status').value);
+            // console.log(document.getElementById('status').isChecked)
 
-
-
-            bookTitle.textContent = formDataAcquired.title;
-            bookAuthor.textContent = formDataAcquired.author;
-            bookPages.textContent = formDataAcquired.pages;
-
-
-
-            if (formBookStatusInput.checked) {
-                bookStatus.textContent = "Read";
-            } else {
-                bookStatus.textContent = "Not Read";
-            }
-
-            console.log(formDataAcquired.status);
-
-            bookCard.appendChild(bookTitle);
-            bookCard.appendChild(bookAuthor);
-            bookCard.appendChild(bookPages);
-            bookCard.appendChild(bookStatus);
-
-            document.body.appendChild(bookCard);
-
-
-
-            // this.form.style.display = 'none'
-
+            bookCardCreator(formDataAcquired);
         })
-
-
     }
-
-
-
-    // else if () {
-    //     alert('Please submit a book first')
-    // }
-
-
 })
 
 
 function addBookToLibrary() {
-
     myLibrary.forEach(book => {
-        const bookCard = document.createElement('div');
-        const bookTitle = document.createElement('h2')
-        const bookAuthor = document.createElement('h3')
-        const bookPages = document.createElement('p')
-        const bookStatus = document.createElement('p')
-
         const newBook = new Book(book.title, book.author, book.pages, book.status)
-
-
-        bookTitle.textContent = newBook.title;
-        bookAuthor.textContent = newBook.author;
-        bookPages.textContent = newBook.pages;
-
-
-
-        if (book.status) {
-            bookStatus.textContent = "Read";
-        } else {
-            bookStatus.textContent = "Not Read";
-        }
-
-
-
-        bookCard.appendChild(bookTitle);
-        bookCard.appendChild(bookAuthor);
-        bookCard.appendChild(bookPages);
-        bookCard.appendChild(bookStatus);
-
-        document.body.appendChild(bookCard)
-
+        bookCardCreator(newBook);
     });
-
 }
-
 
 addBookToLibrary();
